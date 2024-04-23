ab7031d62ff19a4143a7c6336e7b2631d26dfcc7
denisiorodrigues <denisio@ymail.com>
2024-04-22 21:54:29 -0300

    feat: generate project + estructure folders + add entity

---
+++ src/main/java/br/com/dentech/todojava/TodoJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dentech.todojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaApplication.class, args);
+	}
+
+}
