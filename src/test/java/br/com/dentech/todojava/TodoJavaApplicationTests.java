7ef8e92e10fa1c396fa0e0990b432a23eb2cc428
denisiorodrigues <denisio@ymail.com>
2024-04-23 07:36:05 -0300

    feat: app test

--- src/test/java/br/com/dentech/todojava/TodoJavaApplicationTests.java
+++ src/test/java/br/com/dentech/todojava/TodoJavaApplicationTests.java
@@ -1,13 +1,37 @@
 package br.com.dentech.todojava;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.dentech.todojava.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodoJavaApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("Reunião de equipe", "Discussão dos próximos passos do projeto", false, 2);
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].name").isEqualTo(todo.getName())
+			.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+			.jsonPath("$[0].done").isEqualTo(todo.isDone())
+			.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
+	}
+
+	void testCreateTodoFalure() {
 	}
 
 }
