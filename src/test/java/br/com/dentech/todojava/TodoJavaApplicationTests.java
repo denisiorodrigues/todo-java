9051924ec1c3c94e008ad6819f22e47b66deee1e
denisiorodrigues <denisio@ymail.com>
2024-04-24 08:02:47 -0300

    feat: more test methods

--- src/test/java/br/com/dentech/todojava/TodoJavaApplicationTests.java
+++ src/test/java/br/com/dentech/todojava/TodoJavaApplicationTests.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.dentech.todojava.entity.Todo;
 
+import static br.com.dentech.todojava.TestConstants.TODO;
+import static br.com.dentech.todojava.TestConstants.TODOS;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql("/remove.sql")
 class TodoJavaApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
@@ -41,4 +46,83 @@ class TodoJavaApplicationTests {
 			.expectStatus().isBadRequest();
 	}
 
+	@Sql("/import.sql")
+	@Test
+	public void testUpdateTodoSuccess() {
+		var todo = new Todo(TODO.getId(), TODO.getName() + " Up", TODO.getName() + " Up", !TODO.isDone(),
+				TODO.getPriority() + 1);
+
+		webTestClient
+				.put()
+				.uri("/todos/" + TODO.getId())
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(5)
+				.jsonPath("$[0].name").isEqualTo(todo.getName())
+				.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+				.jsonPath("$[0].done").isEqualTo(todo.isDone())
+				.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
+	}
+
+	@Test
+	public void testUpdateTodoFailure() {
+		var unexinstingId = 1L;
+
+		webTestClient
+				.put()
+				.uri("/todos/" + unexinstingId)
+				.bodyValue(
+						new Todo(unexinstingId, "", "", false, 0))
+				.exchange()
+				.expectStatus().isBadRequest();
+	}
+
+	@Sql("/import.sql")
+	@Test
+	public void testDeleteTodoSuccess() {
+		webTestClient
+				.delete()
+				.uri("/todos/" + TODOS.get(0).getId())
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(4)
+				.jsonPath("$[0].name").isEqualTo(TODOS.get(1).getName())
+				.jsonPath("$[0].description").isEqualTo(TODOS.get(1).getDescription())
+				.jsonPath("$[0].done").isEqualTo(TODOS.get(1).isDone())
+				.jsonPath("$[0].priority").isEqualTo(TODOS.get(1).getPriority());
+	}
+
+	@Test
+	public void testDeleteTodoFailure() {
+		var unexinstingId = 1L;
+
+		webTestClient
+				.delete()
+				.uri("/todos/" + unexinstingId)
+				.exchange()
+				.expectStatus().isBadRequest();
+	}
+
+	@Sql("/import.sql")
+	@Test
+	public void testListTodos() throws Exception {
+		webTestClient
+				.get()
+				.uri("/todos")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(5)
+				.jsonPath("$[0]").isEqualTo(TODOS.get(0))
+				.jsonPath("$[1]").isEqualTo(TODOS.get(1))
+				.jsonPath("$[2]").isEqualTo(TODOS.get(2))
+				.jsonPath("$[3]").isEqualTo(TODOS.get(3))
+				.jsonPath("$[4]").isEqualTo(TODOS.get(4));
+	}
 }
